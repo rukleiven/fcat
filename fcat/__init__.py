53bdce128abe3296088e4d8001783a415a941a84
Ruben Kleiven <kleiven.rk@gmail.com>
2021-01-22 16:16:22 +0100

    Add skywalkerX8 coefficients data

--- fcat/__init__.py
+++ fcat/__init__.py
@@ -1,8 +1,9 @@
 from .state_vector import *
 from .control_input_vector import *
-from .aircraft_properties import *
 from .multidim_poly import *
+from .aircraft_properties import *
+from .skywalker8 import *
 
 
 __all__ = (state_vector.__all__ + aircraft_properties.__all__ + control_input_vector.__all__ +
-           multidim_poly.__all__)
+           multidim_poly.__all__ + skywalker8.__all__)
---
+++ fcat/skywalkerX8_data/ctrl_coeff_data.py
@@ -0,0 +1,67 @@
+
+__all__ = ('C_D_delta_e_data', 'C_L_delta_e_data', 'C_m_delta_e_data', 'C_Y_delta_a_data',
+           'C_Y_delta_r_data', 'C_l_delta_a_data', 'C_l_delta_r_data', 'C_n_delta_a_data',
+           'C_n_delta_r_data')
+# Control Coefficients
+C_L_delta_e_data = [  # Data points in alpha (degree), icing, C_L_delta_e(alpha, icing) format:
+    [0, 0, 0.278],
+    [1, 0, 0.278],
+    [0, 1, 0.278],
+    [1, 1, 0.278]
+]
+
+C_D_delta_e_data = [  # Data points in alpha (degree), icing, C_D_delta_e(alpha, icing) format:
+    [0, 0, 0.0633],
+    [1, 0, 0.0633],
+    [0, 1, 0.0633],
+    [1, 1, 0.0633]
+]
+
+C_m_delta_e_data = [  # Data points in alpha (degree), icing, C_m_delta_e(alpha, icing) format:
+    [0, 0, -0.206],
+    [1, 0, -0.206],
+    [0, 1, -0.206],
+    [1, 1, -0.206]
+]
+
+C_Y_delta_a_data = [  # Data points in beta (degree), icing, C_Y_delta_a(beta, icing) format:
+    [0, 0, 0.0433],
+    [1, 0, 0.0433],
+    [0, 1, 0.0433],
+    [1, 1, 0.0433]
+]
+
+C_Y_delta_r_data = [  # Data points in beta (degree), icing, C_Y_delta_r(beta, icing) format:
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+]
+
+C_l_delta_a_data = [  # Data points in beta (degree), icing, C_l_delta_a(beta, icing) format:
+    [0, 0, 0.12],
+    [1, 0, 0.12],
+    [0, 1, 0.12],
+    [1, 1, 0.12]
+]
+
+C_l_delta_r_data = [  # Data points in beta (degree), icing, C_Y_delta_r(beta, icing) format:
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+]
+
+C_n_delta_a_data = [  # Data points in beta (degree), icing, C_n_delta_a(beta, icing) format:
+    [0, 0, -0.00339],
+    [1, 0, -0.00339],
+    [0, 1, -0.00339],
+    [1, 1, -0.00339]
+]
+
+C_n_delta_r_data = [  # Data points in beta (degree), icing, C_n_delta_r(beta, icing) format:
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+]
