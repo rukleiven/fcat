8f1bc12c215eea23daca332faa945164299d2453
Ruben Kleiven <kleiven.rk@gmail.com>
2021-01-31 21:18:51 +0100

    Remove doc-string in inherited classes

--- fcat/frictionless_ball.py
+++ fcat/frictionless_ball.py
@@ -15,130 +15,43 @@ class FrictionlessBall(AircraftProperties):
         super().__init__(control_input)
 
     def drag_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*C_drag*v^2
-
-        where rho is the air density and C_drag is the drag coefficient returned by this function
-        """
         return 0.0
 
     def lift_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        F = 0.5*rho*C_lift*v^2
-
-        where rho is the air density and C_lift is the lift coefficient returned by this function
-        """
         return 0.0
 
     def side_force_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*C_side_force*v^2
-
-        where rho is the air density and C_side_force is the side force coefficient
-        returned by this function
-        """
         return 0.0
 
     def roll_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-       :param state: State vector of the air-craft
-       :param wind: Wind speed vector
-
-       The drag force is given by
-
-       F = 0.5*rho*C_l*v^2
-
-       where rho is the air density and C_l is the rolling moment coefficient
-       returned by this function
-       """
         return 0.0
 
     def pitch_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        F = 0.5*rho*C_m*v^2
-
-        where rho is the air density and C_m is the pitching moment coefficient
-        returned by this function
-        """
         return 0.0
 
     def yaw_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        F = 0.5*rho*C_n*v^2
-
-        where rho is the air density and C_n is the yawing moment coefficient
-        returned by this function
-        """
         return 0.0
 
     def mass(self) -> float:
-        """
-        Aircraft mass
-        """
         return 2
 
     def inertia_matrix(self):
         return 2.0*np.eye(3)
 
     def wing_area(self) -> float:
-        """
-        Return wing area
-        """
         return 2
 
     def mean_chord(self) -> float:
-        """
-        Return mean chord of the wing
-        """
         return 3
 
     def wing_span(self) -> float:
-        """
-        Return wing span
-        """
         return 4
 
     def propeller_area(self) -> float:
-        """
-        Return area swept by propeller
-        """
         return 0.0
 
     def motor_constant(self) -> float:
-        """
-        Return motor constant
-        """
         return 40
 
     def motor_efficiency_fact(self) -> float:
-        """
-        Return motor efficiency factor
-        """
         return 1
--- fcat/simple_aircrafts.py
+++ fcat/simple_aircrafts.py
@@ -25,137 +25,51 @@ class SimpleTestAircraftNoMoments(AircraftProperties):
         super().__init__(control_input)
 
     def drag_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_drag*v^2
-        where rho is the air density and C_drag is the drag coefficient returned by this function
-        """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
         # Test func: F_drag = abs(delta_e)-> C_drag = (2/(wing_area*RHO*V_a^2)* abs(delta_e)
         return 2/(self.wing_area()*RHO*V_a**2)*np.abs(self.control_input.elevator_deflection)
 
     def lift_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_lift*v^2
-
-        where rho is the air density and C_lift is the lift coefficient returned by this function
-        """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
-        # Test func: F_lift = delta_e -> C_lift = 2/(wing_area*RHO*V_a^2)
+        # Test func: F_lift = delta_a -> C_lift = 2/(wing_area*RHO*V_a^2)
         return 2/(self.wing_area()*RHO*V_a**2)*self.control_input.aileron_deflection
 
     def side_force_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_side_force*v^2
-
-        where rho is the air density and C_side_force is the side force coefficient
-        returned by this function
-        """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
+        # Test func: F_side_force = delta_r -> C_lift = 2/(wing_area*RHO*V_a^2)
         return 2/(self.wing_area()*RHO*V_a**2)*self.control_input.rudder_deflection
 
     def roll_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-       :param state: State vector of the air-craft
-       :param wind: Wind speed vector
-
-       The drag force is given by
-
-       TAU = 0.5*rho*wing_area*wing_span*C_l*v^2
-
-       where rho is the air density and C_l is the rolling moment coefficient
-       returned by this function
-       """
         return 0
 
     def pitch_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        TAU = 0.5*rho*wing_area*mean_chord*C_m*v^2
-
-        where rho is the air density and C_m is the pitching moment coefficient
-        returned by this function
-        """
         return 0
 
     def yaw_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-         TAU = 0.5*rho*wing_area*wing_span*C_n*v^2
-
-        where rho is the air density and C_n is the yawing moment coefficient
-        returned by this function
-        """
         return 0
 
     def mass(self) -> float:
-        """
-        Aircraft mass
-        """
         return 2
 
     def inertia_matrix(self):
         return 2.0*np.eye(3)
 
     def wing_area(self) -> float:
-        """
-        Return wing area
-        """
         return 2
 
     def mean_chord(self) -> float:
-        """
-        Return mean chord of the wing
-        """
         return 3
 
     def wing_span(self) -> float:
-        """
-        Return wing span
-        """
         return 4
 
     def propeller_area(self) -> float:
-        """
-        Return area swept by propeller
-        """
         return 1
 
     def motor_constant(self) -> float:
-        """
-        Return motor constant
-        """
-
         return 2
 
     def motor_efficiency_fact(self) -> float:
-        """
-        Return motor efficiency factor
-        """
         return 2/RHO
 
 
@@ -181,100 +95,32 @@ class SimpleTestAircraftNoForces(AircraftProperties):
         self.I_xz = 1
 
     def drag_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_drag*v^2
-        where rho is the air density and C_drag is the drag coefficient returned by this function
-        """
         return 0
 
     def lift_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_lift*v^2
-
-        where rho is the air density and C_lift is the lift coefficient returned by this function
-        """
         return 0
 
     def side_force_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-
-        The drag force is given by
-
-        F = 0.5*rho*wing_area*C_side_force*v^2
-
-        where rho is the air density and C_side_force is the side force coefficient
-        returned by this function
-        """
         return 0
 
     def roll_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-       :param state: State vector of the air-craft
-       :param wind: Wind speed vector
-
-       The drag force is given by
-
-       TAU = 0.5*rho*wing_area*wing_span*C_l*v^2
-
-       where rho is the air density and C_l is the rolling moment coefficient
-       returned by this function
-       """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
-        # Test func: F_drag = abs(delta_e)-> C_drag = (2/(wing_area*RHO*V_a^2)*delta_e
+        # Test func: Roll_moment = delta_e-> C_roll_moment = (2/(wing_area*wing_span*RHO*V_a^2)*delta_e
         return 2/(self.wing_area()*self.wing_span()*RHO*V_a**2) *\
             self.control_input.elevator_deflection
 
     def pitch_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-        TAU = 0.5*rho*wing_area*mean_chord*C_m*v^2
-
-        where rho is the air density and C_m is the pitching moment coefficient
-        returned by this function
-        """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
-        # Test func: F_drag = abs(delta_e)-> C_drag = (2/(wing_area*RHO*V_a^2)*delta_e
+        # Test func: Pitch_moment = delta_a -> C_pitch_moment = (2/(wing_area*mean_chord*RHO*V_a^2)*delta_a
         return 2/(self.wing_area()*self.mean_chord()*RHO*V_a**2) *\
             self.control_input.aileron_deflection
 
     def yaw_moment_coeff(self, state: State, wind: np.ndarray) -> float:
-        """
-        :param state: State vector of the air-craft
-        :param wind: Wind speed vector
-
-        The drag force is given by
-
-         TAU = 0.5*rho*wing_area*wing_span*C_n*v^2
-
-        where rho is the air density and C_n is the yawing moment coefficient
-        returned by this function
-        """
         V_a = np.sqrt(np.sum(calc_airspeed(state, wind)**2))
-        # Test func: F_drag = delta_e -> C_drag = (2/(wing_area*RHO*V_a^2)* abs(delta_e)
+        # Test func: yaw_moment = delta_r -> C_yaw_moment = (2/(wing_area*RHO*wing_span*V_a^2)*delta_r
         return 2/(self.wing_area()*self.wing_span()*RHO*V_a**2)*self.control_input.rudder_deflection
 
     def mass(self) -> float:
-        """
-        Aircraft mass
-        """
         return 2
 
     def inertia_matrix(self):
@@ -283,38 +129,19 @@ class SimpleTestAircraftNoForces(AircraftProperties):
                          [self.I_xz, self.I_yz, self.I_zz]])
 
     def wing_area(self) -> float:
-        """
-        Return wing area
-        """
         return 2
 
     def mean_chord(self) -> float:
-        """
-        Return mean chord of the wing
-        """
         return 3
 
     def wing_span(self) -> float:
-        """
-        Return wing span
-        """
         return 4
 
     def propeller_area(self) -> float:
-        """
-        Return area swept by propeller
-        """
         return 0  # Propulsion Force= 0
 
     def motor_constant(self) -> float:
-        """
-        Return motor constant
-        """
-
         return 0
 
     def motor_efficiency_fact(self) -> float:
-        """
-        Return motor efficiency factor
-        """
         return 2/RHO
