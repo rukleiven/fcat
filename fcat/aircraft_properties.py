a9eb09d77a9aabcc0d9ef69fb2180e087ff844c2
rukleiven <36205731+rukleiven@users.noreply.github.com>
2021-02-01 15:14:40 +0100

    Update Doc-strings/variable names (#15)

--- fcat/aircraft_properties.py
+++ fcat/aircraft_properties.py
@@ -22,11 +22,11 @@ class AircraftProperties(ABC):
     def drag_coeff(self, state: State, wind: np.ndarray) -> float:
         """
         :param state: State vector of the air-craft
-        :param wind: Wind speed vector
+        :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-
-        The drag force is given by
-
+        The drag force is given by;
         F = 0.5*rho*wing_area*C_drag*v^2
 
         where rho is the air density and C_drag is the drag coefficient returned by this function
@@ -36,9 +36,11 @@ class AircraftProperties(ABC):
     def lift_coeff(self, state: State, wind: np.ndarray) -> float:
         """
         :param state: State vector of the air-craft
-        :param wind: Wind speed vector
+        :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-        The drag force is given by
+        The lift force is given by
 
         F = 0.5*rho*wing_area*C_lift*v^2
 
@@ -49,10 +51,11 @@ class AircraftProperties(ABC):
     def side_force_coeff(self, state: State, wind: np.ndarray) -> float:
         """
         :param state: State vector of the air-craft
-        :param wind: Wind speed vector
+        :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-
-        The drag force is given by
+        The side force is given by
 
         F = 0.5*rho*wing_area*C_side_force*v^2
 
@@ -64,9 +67,11 @@ class AircraftProperties(ABC):
     def roll_moment_coeff(self, state: State, wind: np.ndarray) -> float:
         """
        :param state: State vector of the air-craft
-       :param wind: Wind speed vector
+       :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-       The drag force is given by
+       The roll moment is given by
 
        F = 0.5*rho*wing_area*wing_span*C_l*v^2
 
@@ -78,9 +83,11 @@ class AircraftProperties(ABC):
     def pitch_moment_coeff(self, state: State, wind: np.ndarray) -> float:
         """
         :param state: State vector of the air-craft
-        :param wind: Wind speed vector
+        :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-        The drag force is given by
+        The pitch moment is given by
 
         F = 0.5*rho*wing_area*mean_chord*C_m*v^2
 
@@ -92,9 +99,11 @@ class AircraftProperties(ABC):
     def yaw_moment_coeff(self, state: State, wind: np.ndarray) -> float:
         """
         :param state: State vector of the air-craft
-        :param wind: Wind speed vector
+        :param wind: Wind vector: Array of 6 elements, containing the wind contribution
+            to aircraft translational and angular velocity in body frame on form
+            [v_wx, v_wy, v_wz, ang_rate_w_x, ang_rate_w_y, ang_rate_w_z]
 
-        The drag force is given by
+        The yaw moment is given by
 
         F = 0.5*rho*wing_area*wing_span*C_n*v^2
 
@@ -105,7 +114,7 @@ class AircraftProperties(ABC):
     @abstractmethod
     def mass(self) -> float:
         """
-        Aircraft mass
+        Return the aircraft mass
         """
 
     def inv_inertia_matrix(self) -> np.ndarray:
@@ -149,17 +158,32 @@ class AircraftProperties(ABC):
     @abstractmethod
     def propeller_area(self) -> float:
         """
-        Return area swept by propeller
+        The propulsion force in body frame is given by;
+        F_p = 0.5*rho*S_p*C_p*([(k_m*delta_t)^2-V_a^2, 0, 0])
+
+        Where rho is air density, C_p is motor efficiency factor, k_m is motor constant,
+        delta_t is throttle input, V_a is airspeed and
+        S_p is the area swept by the propeller returned by this function
         """
 
     @abstractmethod
     def motor_constant(self) -> float:
         """
-        Return motor constant
+        The propulsion force in body frame is given by;
+        F_p = 0.5*rho*S_p*C_p*([(k_m*delta_t)^2-V_a^2, 0, 0])
+
+        Where rho is air density, C_p is motor efficiency factor,
+        S_p is the area swept by the propeller, delta_t is throttle input, V_a is airspeed and
+        k_m is the motor constant returned by this function
         """
 
     @abstractmethod
     def motor_efficiency_fact(self) -> float:
         """
-        Return motor efficiency factor
+        The propulsion force in body frame is given by;
+        F_p = 0.5*rho*S_p*C_p*([(k_m*delta_t)^2-V_a^2, 0, 0])
+
+        Where rho is air density, k_m is the motor constant,
+        S_p is the area swept by the propeller, delta_t is throttle input, V_a is airspeed and
+        C_p is motor efficiency factor returned by this function
         """
