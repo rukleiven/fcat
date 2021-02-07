05542f25fb55aa8235098b94e2775fcad1abaac9
davidkleiven <davidkleiven446@gmail.com>
2021-02-07 13:35:13 +0100

    ENH: Add dryden model (#18)

--- fcat/__init__.py
+++ fcat/__init__.py
@@ -1,6 +1,7 @@
 from .state_vector import *
 from .control_input_vector import *
 from .wind_model import *
+from .dryden import DrydenGust
 from .multidim_poly import *
 from .aircraft_properties import *
 from .skywalker8 import *
@@ -13,4 +14,4 @@ from .simple_aircrafts import *
 __all__ = (state_vector.__all__ + aircraft_properties.__all__ + control_input_vector.__all__ +
            multidim_poly.__all__ + skywalker8.__all__ + frictionless_ball.__all__
            + simple_aircrafts.__all__ + model_builder.__all__ + property_updater.__all__ +
-           wind_model.__all__)
+           wind_model.__all__ + dryden.__all__)
