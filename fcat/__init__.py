290158454d3471a248ee154eb84c8b8543d08f6f
Ruben Kleiven <kleiven.rk@gmail.com>
2021-02-07 14:16:40 +0100

    Add actuator model to fcat

--- fcat/__init__.py
+++ fcat/__init__.py
@@ -9,9 +9,10 @@ from .frictionless_ball import *
 from .property_updater import *
 from .model_builder import *
 from .simple_aircrafts import *
+from .actuator_model_builder import *
 
 
 __all__ = (state_vector.__all__ + aircraft_properties.__all__ + control_input_vector.__all__ +
            multidim_poly.__all__ + skywalker8.__all__ + frictionless_ball.__all__
            + simple_aircrafts.__all__ + model_builder.__all__ + property_updater.__all__ +
-           wind_model.__all__ + dryden.__all__)
+           wind_model.__all__ + dryden.__all__ + actuator_model_builder.__all__)
